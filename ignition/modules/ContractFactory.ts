ef77ba3832d22f8e40be464dd44bb3236265f05e
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-03 23:11:04 +0700

    update deployed

--- ignition/modules/ContractFactory.ts
+++ ignition/modules/ContractFactory.ts
@@ -4,17 +4,23 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const LockModule = buildModule("Deploy", (m) => {
+  const brandNFT = m.contract("BrandNFT");
   const coupon = m.contract("Coupon");
   const brandMetadata = m.contract("BrandMetadata");
-  const contractFactory = m.contract("ContractFactory");
-  const master = m.contract("Master", [coupon, brandMetadata, contractFactory]) //cara mengambil address gimana?
+  const contractFactory = m.contract("ContractFactory", [brandNFT]);
+  const master = m.contract("Master", [coupon, brandMetadata, contractFactory]);
   
   const DEFAUlT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
   m.call(coupon, "grantRole", [DEFAUlT_ADMIN_ROLE, master]);
   m.call(brandMetadata, "grantRole", [DEFAUlT_ADMIN_ROLE, master]);
   m.call(contractFactory, "grantRole", [DEFAUlT_ADMIN_ROLE, master]);
-  
+
+
+  //testing only
+  // m.call(master, "registerBrand", ["TEST", "TST", "0xC6E18dc61ce1B6C90c7315036E1fD43725F4484d", "anjay"])
+  // m.call(master, "approveBrand", ["0xC6E18dc61ce1B6C90c7315036E1fD43725F4484d","0x5CbDc82605906d2fC422991310890B1bEcC8DC3D"]);
+
   return { coupon, brandMetadata, contractFactory, master };
 });
 
