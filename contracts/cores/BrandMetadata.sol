6c3488b718b08f5d664a8627aa793a2ad3a9c6d5
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-05 00:37:06 +0700

    override update to make NFT soulbound

--- contracts/cores/BrandMetadata.sol
+++ contracts/cores/BrandMetadata.sol
@@ -22,14 +22,24 @@ contract BrandMetadata is ERC721Holder, ERC721URIStorage, AccessControl, IBrandM
     function mint(address brandWallet, string memory uri) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Only Admin can mint");
         require(brandIds[brandWallet] > 0, "Already Registered");
-        _safeMint(address(this), BRAND_ID);
+        _safeMint(brandWallet, BRAND_ID);
         _setTokenURI(BRAND_ID, uri);
-        brandIds[brandWallet];
+        brandIds[brandWallet] = BRAND_ID;
         BRAND_ID++;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         return super.tokenURI(tokenId);
+    }
+
+        function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+        address from = _ownerOf(tokenId);
+        require(
+            from == address(0) || to == address(0),
+            "Soulbound: This token is non-transferable."
+        );
+
+        return super._update(to, tokenId, auth);
     }
 
     function supportsInterface(bytes4 interfaceId)
